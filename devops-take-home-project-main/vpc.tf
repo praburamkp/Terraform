52d8d4af4d1c0e8fdf847874a4d9af05e65509a5
praburamkp <praburamkp@gmail.com>
2024-05-14 08:06:18 +0530

    Updated as per acceptance criteria

--- devops-take-home-project-main/vpc.tf
+++ devops-take-home-project-main/vpc.tf
@@ -1,10 +1,12 @@
 module "label_vpc" {
-  source     = "cloudposse/label/null"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.24.0"
   version    = "0.25.0"
   context    = module.base_label.context
   name       = "vpc"
   attributes = ["main"]
 }
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
@@ -20,11 +22,9 @@ resource "aws_vpc" "main" {
 /* Internet gateway for the public subnet */
 resource "aws_internet_gateway" "ig" {
   vpc_id = "${aws_vpc.main.id}"
-  tags = {
-    Name        = "${var.environment}-igw"
-    Environment = "${var.environment}"
-  }
+  tags   = module.label_vpc.tags
 }
+
 /* Elastic IP for NAT */
 resource "aws_eip" "nat_eip" {
   vpc        = true
@@ -35,72 +35,70 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = "${aws_eip.nat_eip.id}"
   subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
   depends_on    = [aws_internet_gateway.ig]
-  tags = {
-    Name        = "nat"
-    Environment = "${var.environment}"
-  }
+  tags          = module.label_vpc.tags
 }
+
 /* Public subnet */
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
-  count                   = "${length(var.public_subnets_cidr)}"
-  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
-  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  count                   = length(data.aws_availability_zones.available.names)
+  cidr_block             = "192.170.0.${count.index}.0/24"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
-  tags = {
-    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-      public-subnet"
-    Environment = "${var.environment}"
-  }
+  tags                    = module.label_vpc.tags
 }
+
 /* Private subnet */
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
-  count                   = "${length(var.private_subnets_cidr)}"
-  cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
-  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  count                   = length(data.aws_availability_zones.available.names)
+  cidr_block             = "192.170.0.${count.index}.0/24"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = false
-  tags = {
-    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
-    Environment = "${var.environment}"
-  }
+  tags                    = module.label_vpc.tags
 }
+
 /* Routing table for private subnet */
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.main.id}"
-  tags = {
-    Name        = "${var.environment}-private-route-table"
-    Environment = "${var.environment}"
-  }
+  tags                 = module.label_vpc.tags
 }
+
 /* Routing table for public subnet */
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.main.id}"
-  tags = {
-    Name        = "${var.environment}-public-route-table"
-    Environment = "${var.environment}"
-  }
+  tags                 = module.label_vpc.tags
 }
+
 resource "aws_route" "public_internet_gateway" {
   route_table_id         = "${aws_route_table.public.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.ig.id}"
+  tags                   = module.label_vpc.tags
 }
+
 resource "aws_route" "private_nat_gateway" {
   route_table_id         = "${aws_route_table.private.id}"
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+  tags                   = module.label_vpc.tags
 }
+
 /* Route table associations */
 resource "aws_route_table_association" "public" {
   count          = "${length(var.public_subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
+  tags           = module.label_vpc.tags
 }
+
 resource "aws_route_table_association" "private" {
   count          = "${length(var.private_subnets_cidr)}"
   subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.private.id}"
+  tags           = module.label_vpc.tags
 }
+
 /*==== VPC's Default Security Group ======*/
 resource "aws_security_group" "default" {
   name        = "${var.environment}-default-sg"
@@ -120,7 +118,5 @@ resource "aws_security_group" "default" {
     protocol  = "-1"
     self      = "true"
   }
-  tags = {
-    Environment = "${var.environment}"
-  }
+  tags        = module.label_vpc.tags
 }
